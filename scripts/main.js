0de4e1a1830a44d4501fb72906f9012d6975a856
Mand Cashin <mandcashin@hotmail.co.uk>
2021-06-15 19:16:11 +0100

    feat: sort button functionality for name and date

--- scripts/main.js
+++ scripts/main.js
@@ -8,13 +8,13 @@ function buildTable() {
 
         data.forEach((commit) => {
           tableBody += `
-<tr>
-<td>${commit.commit.author.name}</td>
-<td>${commit.commit.author.date}</td>
-<td>${commit.commit.message}</td>
-<td>${commit.commit.url}</td>
-</tr>
-`;
+            <tr>
+            <td>${commit.commit.author.name}</td>
+            <td>${commit.commit.author.date}</td>
+            <td>${commit.commit.message}</td>
+            <td>${commit.commit.url}</td>
+            </tr>
+            `;
         });
 
         document.getElementById("data").innerHTML = tableBody;
@@ -24,3 +24,57 @@ function buildTable() {
 }
 
 buildTable();
+
+function sortTable(n) {
+  let table,
+    rows,
+    switching,
+    i,
+    x,
+    y,
+    shouldSwitch,
+    dir,
+    switchcount = 0;
+
+  table = document.getElementById("table");
+
+  switching = true;
+
+  dir = "asc";
+
+  while (switching) {
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+
+    for (i = 1; i < rows.length - 1; i++) {
+      shouldSwitch = false;
+
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          shouldSwitch = true;
+          break;
+        }
+      }
+    }
+
+    if (shouldSwitch) {
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+
+      switchcount++;
+    } else {
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
