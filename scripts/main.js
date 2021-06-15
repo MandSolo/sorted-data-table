3ae93f8cb8ad5be1d96cdd53fc564f0b48241092
Mand Cashin <mandcashin@hotmail.co.uk>
2021-06-15 19:08:39 +0100

    feat: build table function

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,26 @@
-fetch("https://api.github.com/repositories/19438/commits").then((res) => {
-  res.json().then((data) => {
-    console.log(data);
+function buildTable() {
+  fetch("https://api.github.com/repositories/19438/commits").then((res) => {
+    res.json().then((data) => {
+      console.log(data);
+
+      if (data.length > 0) {
+        let tableBody = "";
+
+        data.forEach((commit) => {
+          tableBody += `
+<tr>
+<td>${commit.commit.author.name}</td>
+<td>${commit.commit.author.date}</td>
+<td>${commit.commit.message}</td>
+<td>${commit.commit.url}</td>
+</tr>
+`;
+        });
+
+        document.getElementById("data").innerHTML = tableBody;
+      }
+    });
   });
-});
+}
+
+buildTable();
